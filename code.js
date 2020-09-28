aa56457b8805de6e120b49881b7652ee75cebb39
Jonathan Lönnqvist <jonathan.lonnqvist@skola.botkyrka.se>
2020-09-28 15:19:29 +0200

    tiny improvements

---
+++ code.js
@@ -0,0 +1,8 @@
+let button = document.createElement("button")
+let body = document.getElementsByTagName("body")
+body.apendChild(button)
+
+
+function button(onclick){
+    alert("you clicked the unclickable button")
+}
